69b8ed1cdb7a0067239fc293f70d353072589e85
Boris-Tang <boris.tang@deltaprojects.com>
2023-10-09 15:35:21 +0200

    remove unused import

--- server/src/main/java/se/sperber/cryson/repository/CrysonRepository.java
+++ server/src/main/java/se/sperber/cryson/repository/CrysonRepository.java
@@ -50,7 +50,6 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
-import java.util.function.Supplier;
 
 @Repository
 public class CrysonRepository {
